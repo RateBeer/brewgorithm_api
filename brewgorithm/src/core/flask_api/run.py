7c5df8376ad113c8e258aaa4423368a9c24fb695
Patrick Scott <patrick.scott@zx-ventures.com>
2017-11-20 14:35:28 -0500

    add statusCodes to responses from API

--- brewgorithm/src/core/flask_api/run.py
+++ brewgorithm/src/core/flask_api/run.py
@@ -14,11 +14,11 @@ beers_map = {}
 def get_descriptor(ratebeer_id):
   """For a text query, pipe it through the gate and return the best answer."""
   if int(ratebeer_id) not in beers_map:
-    return jsonify({'response': None})
+    return jsonify({'response': None, 'statusCode': 500, 'error': "ratebeer_id " + ratebeer_id + " not found"})
   beer = beers_map[int(ratebeer_id)]
   descriptors = [x[0] for x in beer_emb.most_similar(positive=[beer['vector']])]
   unique_descriptors = beer_emb.remove_duplicates(descriptors)
-  return jsonify({'response': unique_descriptors})
+  return jsonify({'descriptors': unique_descriptors, 'statusCode': 200})
 
 
 @app.route("/recommend", methods=['POST'])
@@ -28,10 +28,10 @@ def get_recommendations():
   try:
     assert('ids' in content)
     assert(len(content['ids']) > 0)
-    return jsonify({'response': recommender.similar_beers([
+    return jsonify({'statusCode': 200, 'response': recommender.similar_beers([
         int(x) for x in content['ids']], beers_map, beer_emb.EMB_DIM)})
   except (KeyError, AssertionError):
-    return jsonify({'response': None})
+    return jsonify({'response': None, 'statusCode': 500})
 
 
 if __name__ == "__main__":
