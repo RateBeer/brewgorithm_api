c118314a2d3ac5a737f61c92e7751d6463ce284b
Patrick Scott <patrick.scott@zx-ventures.com>
2017-12-12 10:54:36 -0500

    update db location

--- brewgorithm/src/data_pipelines/config.py
+++ brewgorithm/src/data_pipelines/config.py
@@ -1,5 +1,5 @@
 import os
 
-SQL_SERVER = "ratebeer-sql-web.cabeeuukfc9z.us-west-2.rds.amazonaws.com"
+SQL_SERVER = "ratebeer-sql-web-qa.cabeeuukfc9z.us-west-2.rds.amazonaws.com"
 DATABASE = "Ratebeer2"
 MODEL_DIR = os.path.dirname(os.path.realpath(__file__)) + "/storage/"
