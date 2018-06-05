e9984dd3323244d57a8e91ca4edab0bc01fe6bd0
Alex Tween <alextween@gmail.com>
2018-06-04 21:11:46 -0400

    fix for training

--- brewgorithm/src/neural/word_weighter/load.py
+++ brewgorithm/src/neural/word_weighter/load.py
@@ -1,8 +1,8 @@
 from keras.models import load_model
 from .config import MODEL_DIR, MODEL_NAME, EMB_DIM
-
+import tensorflow as tf
 model = load_model(MODEL_DIR + MODEL_NAME)
-
+graph = tf.get_default_graph()
 
 def get_model():
   """Return the keras model used for beer relatedness classification."""
@@ -14,5 +14,6 @@ def is_beer_related(word_vector, threshold=0.8):
   # when the word_vector is a zero vector
   if not word_vector.any():
     return False
-  score = model.predict(word_vector.reshape((-1, EMB_DIM)))[0][0]
+  with graph.as_default():
+    score = model.predict(word_vector.reshape((-1, EMB_DIM)))[0][0]
   return score > threshold
