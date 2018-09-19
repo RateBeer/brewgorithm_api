40b58813f770a37727e9a821ab5a33f6e93e3aca
TJ (Thomas J.) Biddle <biddle.thomas@gmail.com>
2018-09-19 23:18:33 +0800

    Setting up Travis CI + Other small changes (#4)
    * Trying on dev branch; never downloaded original - replacing with fake
    
    * sort descriptors (#1)
    
    * Remove the local endpoint setup for now as it's causing problems and we do not have it setup yet anyhow.
    
    * Busting Docker cache.
    
    * Busting cache.
    
    * Busting cache..
    
    * Brewgorithm training 2018-09-04
    
    * Switching to TravisCI.

---
+++ travis/deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -ev
+
+case "$TRAVIS_BRANCH" in
+"dev")
+    AWS_ECS_REGION=${DEV_AWS_ECS_REGION}
+    AWS_ECS_CLUSTER=${DEV_AWS_ECS_CLUSTER}
+    ;;
+"master")
+    AWS_ECS_REGION=${MASTER_AWS_ECS_REGION}
+    AWS_ECS_CLUSTER=${MASTER_AWS_ECS_CLUSTER}
+    ;;
+*)
+    exit 1; # Unsupported deployment branch.
+    ;;
+esac
+
+IMAGE_NAME=brewgorithm-api-${TRAVIS_BUILD_NUMBER}-prod
+VERSION_TAG="${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}"
+LATEST_TAG="${TRAVIS_BRANCH}-latest"
+IMAGE_NAME_WITH_VERSION_TAG="${DOCKER_IMAGE_NAME}:${VERSION_TAG}"
+IMAGE_NAME_WITH_LATEST_TAG="${DOCKER_IMAGE_NAME}:${LATEST_TAG}"
+
+# Build the Production-ready image
+docker build -t ${IMAGE_NAME} .
+
+# Instal AWS CLI w/o sudo
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+# Login to AWS ECR
+eval $(aws ecr get-login --region ${AWS_ECR_REGION} --no-include-email)
+
+# Create Docker Tags
+docker tag ${IMAGE_NAME} ${AWS_ECR_ACCOUNT_ID}.dkr.ecr.${AWS_ECR_REGION}.amazonaws.com/${IMAGE_NAME_WITH_VERSION_TAG}
+docker tag ${IMAGE_NAME} ${AWS_ECR_ACCOUNT_ID}.dkr.ecr.${AWS_ECR_REGION}.amazonaws.com/${IMAGE_NAME_WITH_LATEST_TAG}
+
+# Push up the image
+docker push ${AWS_ECR_ACCOUNT_ID}.dkr.ecr.${AWS_ECR_REGION}.amazonaws.com/${IMAGE_NAME_WITH_VERSION_TAG}
+docker push ${AWS_ECR_ACCOUNT_ID}.dkr.ecr.${AWS_ECR_REGION}.amazonaws.com/${IMAGE_NAME_WITH_LATEST_TAG}
+
+# Deploy the image with ECS-deploy; roll back upon failure.
+docker run -e AWS_DEFAULT_REGION=${AWS_ECS_REGION} -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} silintl/ecs-deploy --use-latest-task-def -c ${AWS_ECS_CLUSTER} -n ${AWS_ECS_SERVICE_NAME} -to ${VERSION_TAG} -i ignore -t 3600 -r ${AWS_ECS_REGION} --enable-rollback
---
+++ travis/tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ev
+
+# Tests are baked into the image.
+
+exit 0;
